290e6dd2631890bcf411ceac95b6e2cc6e26692e
uday <udaythird@gmail.com>
2022-11-23 16:49:53 -0500

    added alb header routing

---
+++ terraform-ec2/12-ALB-qs-header/README.md
@@ -0,0 +1,23 @@
+Tested and working as expected.
+
+updated ALB module to latest
+
+https://restninja.io/
+
+1. https://apps.devopsdemo.tk
+
+# Rule 1 (REST API)
+2. custom-header = app-1
+custom-header = app1
+custom-header = my-app-1
+
+# Rule 2 (REST API)
+3. custom-header = app-2
+custom-header = app2
+custom-header = my-app-2
+
+# Rule 3 (from browser)
+4. apps.devopsdemo.tk/?website=aws-eks
+
+# Rule 4 (from browser)
+5. azure-aks101.devopsdemo.tk
---
+++ terraform-ec2/12-ALB-qs-header/c11-acm-certificatemanager.tf
@@ -0,0 +1,24 @@
+
+# ACM Module - To create and Verify SSL Certificates
+module "acm" {
+  source  = "terraform-aws-modules/acm/aws"
+  version = "~> 4.0"
+
+  domain_name = trimsuffix(data.aws_route53_zone.mydomain.name, ".")
+  zone_id     = data.aws_route53_zone.mydomain.zone_id
+
+  subject_alternative_names = [
+    "*.devopsdemo.tk",
+  ]
+
+  # default is true  
+  wait_for_validation = true
+
+  tags = local.common_tags
+}
+
+# Output ACM Certificate ARN
+output "acm_certificate_arn" {
+  description = "ACM Certificate ARN"
+  value       = module.acm.acm_certificate_arn
+}
