7e0ba9fb323cbe002e9b7264bfafd4521b69f764
uday <udaythird@gmail.com>
2023-01-04 19:23:37 -0500

    updated bastion files

--- terraform-azure/12-Azure-Bastion-Service-and-Host/terraform-manifests/c8-01-bastion-host-input-variables.tf
+++ terraform-azure/12-Azure-Bastion-Service-and-Host/terraform-manifests/c8-01-bastion-host-input-variables.tf
@@ -1,11 +1,11 @@
 # Bastion Linux VM Input Variables Placeholder file.
 variable "bastion_service_subnet_name" {
   description = "Bastion Service Subnet Name"
-  default = "AzureBastionSubnet"
+  default     = "AzureBastionSubnet"
 }
 
 variable "bastion_service_address_prefixes" {
   description = "Bastion Service Address Prefixes"
-  default = ["10.0.101.0/27"]
+  default     = ["10.0.101.0/27"]
 }
 
