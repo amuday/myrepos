5030d6efa64c062f6ca919e76d35e052d6ebc2e9
uday <udaythird@gmail.com>
2023-03-15 15:28:23 -0400

    added ec2

--- terraform-testing/01-conf-test/ec2.tf
+++ terraform-testing/01-conf-test/ec2.tf
@@ -1,6 +1,6 @@
 
 resource "aws_instance" "web" {
-  subnet_id     = "subnet-private"
+  subnet_id     = "subnet-public"
   ami           = "ami-026b57f3c383c2eec"
   instance_type = "m7g.8xlarge"
 }
